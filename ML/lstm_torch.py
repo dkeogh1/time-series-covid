3537ce4ac46dbcde830cf9bd2f3959b6b303e052
dkeogh1 <danielkeogh@Daniels-MacBook-Pro.local>
2020-03-22 10:39:16 -0700

    data gen nbs

--- ML/lstm_torch.py
+++ ML/lstm_torch.py
@@ -6,22 +6,9 @@ from torch import nn, optim
 
 from sklearn.preprocessing import MinMaxScaler
 
-
-def set_seq(d, seq_len: int):
-    xi = []
-    yi = []
-
-    for i in range(len(d) - seq_len):
-        x = d[i:(i+seq_len)]
-        y = d[i:+seq_len]
-        xi.append(x)
-        yi.append(y)
-
-    return np.array(xi), np.array(yi)
-
 class LSTM_Predictor(nn.Module):
 
-    def __init__(self, features: int, neurons: int, sequences: int, layers: int, dropout: float):
+    def __init__(self, features, neurons, sequences, layers, dropout):
         super(LSTM_Predictor, self).__init__()
 
         self.neurons = neurons
@@ -31,27 +18,30 @@ class LSTM_Predictor(nn.Module):
         self.lstm = nn.LSTM(
             input_size = features,
             hidden_size = neurons,
-            layers = layers,
+            num_layers = layers,
             dropout = dropout)
 
         self.linear = nn.Linear(in_features = neurons, 
                                 out_features=1)
 
-        def hidden_reset(self):
-            self.hidden = (
-                torch.zeros(self.layers, self.sequences, self.neurons),
-                torch.zeros(self.layers, self.sequences, self.neurons)
-            )
+    def hidden_reset(self):
+        self.hidden = (
+            torch.zeros(self.layers, self.sequences, self.neurons),
+            torch.zeros(self.layers, self.sequences, self.neurons))
 
-        def forward(self, seqs: int):
-            lstm_o, self.neurons = self.lstm(seq.view(len(seqs),self.sequences, -1), self.neurons)
-            last_step = lstm_o.view(self.sequences, len(seqs), self.neurons)[-1]
-            pred_out = self.linear(last_step)
-            return pred_out
+    def forward(self, seqs):
+        lstm_out, self.hidden = self.lstm(
+        seqs.view(len(seqs), self.sequences, -1),
+        self.hidden
+        )
+        last_time_step = \
+        lstm_out.view(self.sequences, len(seqs), self.neurons)[-1]
+        y_pred = self.linear(last_time_step)
+        return y_pred
 
 class LSTM_data_loader():
     
-    def __init__(self, region_abr, region_list, state_mapper, country, df, sequence_lenth):
+    def __init__(self, region_abr, region_list, state_mapper, country, df):
         
         self.df = df
         self.region_abr = region_abr
@@ -61,7 +51,6 @@ class LSTM_data_loader():
         self.scaler = MinMaxScaler()
         self.train_data = None
         self.test_data = None
-        self.sequence_lenth = 3
         
     def _remap_to_abr(self, state):
         return self.state_mapper[state]
@@ -73,17 +62,18 @@ class LSTM_data_loader():
             mask1 = self.df['Country/Region'].str.contains(self.country)
             self.df = self.df[mask1]
             
-            for x in self.region_list:
-                counter = 0
-                for r in self.df['Province/State'].tolist():
-                    if x in r:
-                        self.df['Province/State'].iloc[counter] = x
-                    elif r in self.state_mapper.keys():
-                        try:
-                            self.df['Province/State'].iloc[counter] = self._remap_to_abr(r)
-                        except Exception as e:
-                            print(e)
-                    counter += 1
+            if self.region_list:
+                for x in self.region_list:
+                    counter = 0
+                    for r in self.df['Province/State'].tolist():
+                        if x in r:
+                            self.df['Province/State'].iloc[counter] = x
+                        elif r in self.state_mapper.keys():
+                            try:
+                                self.df['Province/State'].iloc[counter] = self._remap_to_abr(r)
+                            except Exception as e:
+                                print(e)
+                        counter += 1
                         
             mask2 = self.df['Province/State'].str.contains(self.region_abr)
             self.df = self.df[mask2]
@@ -99,27 +89,41 @@ class LSTM_data_loader():
         else:
             self.df = self.df.iloc[:,4:]
             
-    def transform_df_datetime(self):
-        self.df = self.df.sum(axis=0)
-        self.df.index = pd.to_datetime(self.df.index)
+    def transform_df_datetime(self, delta=False):
+        if not delta:
+            self.df = self.df.sum(axis=0)
+            self.df.index = pd.to_datetime(self.df.index)
+            print("Data in cumulative")
+
+        else:
+            self.df = self.df.sum(axis=0)
+            self.df.index = pd.to_datetime(self.df.index)
+            self.df = self.df.diff().fillna(self.df[0]).astype(np.int64)
+            print("Data is converted to daily delta")
+
         
     def gen_data_sets(self,test_data_size=0):   
-        self.train_data = self.df[:-test_data_size]
-        self.scaler = self.scaler.fit(np.expand_dims(self.train_data, axis=1))
-        self.train_data = self.scaler.transform(np.expand_dims(self.train_data, axis=1))
-
         if test_data_size:
+            self.train_data = self.df[:-test_data_size]
+            self.scaler = self.scaler.fit(np.expand_dims(self.train_data, axis=1))
+            self.train_data = self.scaler.transform(np.expand_dims(self.train_data, axis=1))
+
             self.test_data = self.df[-test_data_size:]
             self.test_data = self.scaler.transform(np.expand_dims(self.test_data, axis=1))
+
+        else:
+            self.train_data = self.df
+            self.scaler = self.scaler.fit(np.expand_dims(self.train_data, axis=1))
+            self.train_data = self.scaler.transform(np.expand_dims(self.train_data, axis=1))      
         
-    def set_seq(self, train=True):
+    def set_seq(self, train=True, sequence_lenth=3):
         if train:
             xs = []
             ys = []
 
-            for i in range(len(self.train_data)-self.sequence_lenth-1):
-                x = self.train_data[i:(i+self.sequence_lenth)]
-                y = self.train_data[i+self.sequence_lenth]
+            for i in range(len(self.train_data)-sequence_lenth-1):
+                x = self.train_data[i:(i+sequence_lenth)]
+                y = self.train_data[i+sequence_lenth]
                 xs.append(x)
                 ys.append(y)
 
@@ -129,9 +133,9 @@ class LSTM_data_loader():
             xs = []
             ys = []
 
-            for i in range(len(self.test_data)-self.sequence_lenth-1):
-                x = self.test_data[i:(i+self.sequence_lenth)]
-                y = self.test_data[i+self.sequence_lenth]
+            for i in range(len(self.test_data)-sequence_lenth-1):
+                x = self.test_data[i:(i+sequence_lenth)]
+                y = self.test_data[i+sequence_lenth]
                 xs.append(x)
                 ys.append(y)
 
@@ -158,12 +162,29 @@ def train_lstm(lstm, training_data, training_lables, testing_data = None, testin
                 test_loss = loss_function(test_pred.float(), testing_labels)
             testing_history[i] = test_loss.item()
 
-        if i % 5 == 0:
-            print(f'Epoch {i} train loss: {loss.item()} test loss: {test_loss.item()}')
-    
+        if i % 40 == 0:
+            if testing_data is not None:
+                print(f'Epoch {i} train loss: {loss.item()} test loss: {test_loss.item()}')
+            else:
+                print(f'Epoch {i} train loss: {loss.item()}')
         training_history[i] = loss.item()
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
     return lstm.eval(), training_history, testing_history
+
+def predict_future(n_future, time_data, sequece_lenth, model):
+    test_seq = time_data[:1]
+    with torch.no_grad():
+        preds = []
+        for _ in range(n_future):
+            y_test_pred = model(test_seq)
+            pred = torch.flatten(y_test_pred).item()
+            preds.append(pred)
+            new_seq = test_seq.numpy().flatten()
+            new_seq = np.append(new_seq, [pred])
+            new_seq = new_seq[1:]
+            test_seq = torch.as_tensor(new_seq).view(1, sequece_lenth, 1).float()
+
+        return preds
